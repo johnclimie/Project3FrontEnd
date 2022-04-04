58b991320a9d33b81a5a19650ac2003b06fe54f6
John Climie <johnclimie00@gmail.com>
2022-04-04 18:09:24 -0400

    Set up routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Pages
-import Login from '../src/pages/Login'
+import Login from '../src/pages/Login';
+import Home from '../src/pages/Home';
 
 function App() {
     return (
-        <Login />
+        <Router>
+            <Routes>
+                <Route 
+                    exact path="/"
+                    element={<Login />}
+                />
+
+                <Route
+                    exact path="/home"
+                    element={<Home />} 
+                />
+            </Routes>
+        </Router>
+
     );
 }
 
